c40c204a419e1fd34b99f383b64c0e650a8a854c
johnkoo <rndkoo@gmail.com>
2022-02-17 23:10:41 +0900

    v-for

--- toy/src/App.vue
+++ toy/src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div class="menu">
+
+    <a v-for="(a,i) in 메뉴들" :key="i">{{a}} {{i}}</a>
+     
+  
+     
+
+
   </div>
+  
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+<!-- <div>
+  <h4>역삳동</h4>
+  
+  </div>  
+   -->
+ 
+
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+
+
 
 export default {
   name: 'app',
+data(){
+
+return {
+  메뉴들:['Home','Shop','About'],
+  products :['역삼동원룸','천호동원룸','마포구원룸']
+}
+
+},
+
+
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -23,6 +53,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding:10px;
+}
+
+
+
 </style>
