2a8eecd50365b1e5859966d2453714e18f54ca9b
johnkooTri <rndkoo@gmail.com>
2022-02-22 09:16:06 +0900

    img 삽입

--- toy/src/App.vue
+++ toy/src/App.vue
@@ -1,95 +1,87 @@
 <template>
-
-
-
   <div id="app">
-
-    <div class ="menu">
-      <a v-for="(a,i) in menus" :key="i">{{ a }}</a>
+    <div class="menu">
+      <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
     </div>
 
-
-<img :src="imgpath2" class="room-img">
-    <div v-for="(a,i) in products" :key="i">
-      <!-- <img :src="imgpath2" class="room-img"> -->
+    <div v-for="(a, i) in products" :key="i">
+      <img :src="a.img" class="room-img">
       <!-- {{imgpath[i]}} -->
 
-      <!-- <img src="./assets/room0.jpg" class="room-img"> -->
+      <!-- <img src="./assets/room0.jpg" class="room-img" /> -->
 
-      <h4 class="red" :style="style1">{{a}}</h4>
+      <h4 class="red" :style="style1">{{ a.room }}</h4>
       <p>50만원</p>
       <!-- <button v-on:click="callcnt++">허위매물신고</button><span>신고수 : {{ callcnt }}</span> -->
-          <!-- <button @mouseover="increase(i)">허위매물신고</button> -->
-          <button @mouseover="increase(i)">허위매물신고</button>
-          <span>신고수 : {{ callcnt[i] }}</span>
-    </div>      
-
-
-
+      <!-- <button @mouseover="increase(i)">허위매물신고</button> -->
+      <button v-on:click="increase(i)">허위매물신고</button>
+      <span>신고수 : {{ callcnt[i] }}</span>
+    </div>
   </div>
-
-
-
-
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  data(){
-    return{
-      price1:60,
-      price2:70,
-      style1:'color : blue',
-      products:['역삼동원룸','천호동원룸','마포구원룸'],
-      menus:['Home','Shop','About'],
-      callcnt:[0,1,2],
-      imgpath2:'./assets/room0.jpg',
-      imgpath:[
-            './assets/room0.jpg',
-            './assets/room1.jpg',
-            './assets/room2.jpg'
-      ]
-    }
+  name: "app",
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      style1: "color : blue",
+      products: [
+        
+        {
+          room: "역삼동원룸",
+          img:require("./assets/room0.jpg")
+        },
+        {
+          room: "천호동원룸",
+          img:require("./assets/room1.jpg")
+        },
+        {
+          room: "마포구원룸",
+          img:require("./assets/room2.jpg")
+        }
+          
+      ],
+      menus: ["Home", "Shop", "About"],
+      callcnt: [0, 1, 2],
+      imgpath2: "./assets/room0.jpg",
+      imgpath: ["./assets/room0.jpg","./assets/room1.jpg","./assets/room2.jpg"]
+    };
   },
 
-  methods:{
-
-    makeimg(i){
-
-      this.imgpath='./assets/room'+i+'.jpg'
-      console.log(this.imgpath)
+  methods: {
+    makeimg(i) {
+      this.imgpath = "./assets/room" + i + ".jpg";
+      // console.log(this.imgpath);
       this.$forceUpdate();
     },
 
-    increase(i){
-      
+    increase(i) {
       // console.log(i+"   :  번째 데이터 ");
 
       // console.log(this.callcnt[i]+1);
 
-      this.callcnt[i]=this.callcnt[i]+1
+      this.callcnt[i] = this.callcnt[i] + 1;
 
       this.$forceUpdate();
 
       // this.callcnt[i]=4+i;
-
-    }
-
-
+    },
   },
 
   components: {
     // HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -97,23 +89,19 @@ export default {
   margin-top: 60px;
 }
 
-.room-img{
-  width:100%;
-  margin-top:40px;
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 
-
-
-.menu{
-background:darkslateblue;
-padding:15px;
-border-radius: 5px;
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
 }
 
 .menu a {
-  color:white;
-  padding:10px;
+  color: white;
+  padding: 10px;
 }
-
-
 </style>
