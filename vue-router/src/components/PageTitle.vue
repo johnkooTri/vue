cc988a5fa7ee12a1ff40146a978dc8e6bfa22831
johnkooTri <rndkoo@gmail.com>
2022-03-11 23:10:31 +0900

    부모 컴포넌트와 자식 컴포넌트

---
+++ vue-router/src/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>{{title}}</h2>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+
+    props:{
+        title:{
+            type:String,
+            default:"페이지 제목입니다."
+        }
+    }
+
+}
+</script>
--- vue-router/src/views/Home.vue
+++ vue-router/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+<Pagetitle title="부모 컴포넌트에서 자식컴포넌트로 데이터 전달"/>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +10,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Pagetitle from '@/components/PageTitle.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+    ,Pagetitle
   }
 }
 </script>
