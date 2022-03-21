38e4ab0b991c28bbaeac8f15e14c0515a0cc3b12
koojaeun <rndkoo@gmail.com>
2022-03-21 14:07:06 +0900

    slot 사용법

--- vue-router/src/components/PageTitle.vue
+++ vue-router/src/components/PageTitle.vue
@@ -1,25 +1,5 @@
 <template>
-    <h2>{{title}}</h2>
+    <h2><slot></slot></h2>
 </template>
 
-<script>
-// import { defineComponent } from '@vue/composition-api'
-
-export default {
-
-    // props:{
-    //     title:{
-    //         type:String,
-    //         default:"페이지 제목입니다."
-    //     }
-    // }
-    // ,
-    data(){
-         return{
-                title:"동적페이지 타이틀",
-                post:{id:1,title:'vue3'}
-
-               }
-    }
-}
-</script>
+<PageTitle>컴포넌트 사용 예제 페이지</PageTitle>
